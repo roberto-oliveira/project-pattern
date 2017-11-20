673007089a557e09a76b72a6cad93315938dcd93
roberto-oliveira <robertocoliveira@gmail.com>
2017-11-20 12:13:55 -0300

    Update GenericDao.cs

--- GenericBase/GenericDao.cs
+++ GenericBase/GenericDao.cs
@@ -1,22 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.Linq;
-using System.Data.Linq.Mapping;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Linq.Expressions;
 
-namespace GenericBase
+namespace Generics
 {
-    public abstract class GenericDao<TEntity, TDataContext>
-        where TEntity : class where TDataContext : DataContext, new()
+    public abstract class GenericDao<TEntity, TDataContext> where TEntity : class where TDataContext : DataContext, new()
     {
-        private readonly TDataContext _dataContext = new TDataContext();
+        private static readonly TDataContext _dataContext = new TDataContext();
+        private static readonly JavaScriptSerializer jss = new JavaScriptSerializer();
 
         [DataObjectMethod(DataObjectMethodType.Insert, false)]
         public void Insert(TEntity entity)
@@ -47,148 +43,25 @@ namespace GenericBase
             _dataContext.SubmitChanges();
         }
 
-        public virtual ITable GetClass()
+        public IQueryable<TEntity> GetGeneric(Expression<Func<TEntity, bool>> lbdEx)
         {
-            return _dataContext.GetTable<TEntity>();
+            return _dataContext.GetTable<TEntity>().Where(lbdEx).AsQueryable();
         }
 
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public IQueryable<TEntity> GetTable()
+        public virtual string GetGenericReturnJson(Expression<Func<TEntity, bool>> lbdEx)
         {
-            return _dataContext.GetTable<TEntity>();
+            var query = _dataContext.GetTable<TEntity>().Where(lbdEx).AsQueryable();
+            var json = jss.Serialize(query);
+
+            return json;
         }
 
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public IList<TEntity> GetList()
+        public virtual string GetListEntitiesFormatJson(List<TEntity> listEntities)
         {
-            return _dataContext.GetTable<TEntity>().ToList();
+            var query = _dataContext.GetTable<TEntity>().ToList();
+            var json = jss.Serialize(query);
+
+            return json;
         }
-
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public virtual List<TEntity> GetNormalList()
-        {
-            return _dataContext.GetTable<TEntity>().ToList();
-        }
-
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<TEntity> GetNormalListWithParameters(int mes, int ano)
-        {
-            return _dataContext.GetTable<TEntity>().ToList();
-        }
-
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public IEnumerable<TEntity> GetListIEnumerable()
-        {
-            return _dataContext.GetTable<TEntity>().ToList();
-        }
-
-
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public TEntity SearchById(int id)
-        {
-            var parametro = Expression.Parameter(typeof(TEntity), "item");
-
-            var expressao = Expression.Lambda<Func<TEntity, bool>>(Expression.Equal(Expression.Property(parametro,
-                    typeof(TEntity)
-                        .GetPrimaryKey().Name),
-                Expression.Constant(id)), parametro);
-
-            var item = GetTable().Where(expressao).SingleOrDefault();
-
-            return item;
-        }
-        // Func recebe a entidade/classe (ex produto)
-        // a ser usada na pesquisa, portanto, é dinâmica
-
-        //public IQueryable<TEntity> Get(Func<TEntity, bool> predicate)
-        //{
-        //    //TEntity = é uma classe, ex Produtos, Clientes
-        //    // predicate = é a expressão de filtro
-        //    // p => p.Preco > 10
-        //    // AsQueryable = converte para uma lista consultável
-        //    // .Set<> referencia a entidade dinamicamente
-        //    return ctx.Set<TEntity>().Where(predicate).AsQueryable();
-        //}
-
-        public IQueryable<TEntity> ConsultaGenerica(Func<TEntity, bool> expressionLambda)
-        {
-            return _dataContext.GetTable<TEntity>().Where(expressionLambda).AsQueryable();
-        }
-
-    }
-
-    public static class Auxiliar
-    {
-        public static PropertyInfo GetPrimaryKey(this Type entityType)
-        {
-            foreach (var property in entityType.GetProperties().Where(property => property.IsPrimaryKey()))
-            {
-                if (property.PropertyType != typeof(int))
-                {
-                    throw new Exception($"Primary key, '{property.Name}', do tipo '{entityType}' não é int");
-                }
-                return property;
-            }
-            throw new ApplicationException($"Sem primary key definida para o tipo '{entityType.Name}'");
-        }
-
-        public static bool IsPrimaryKey(this PropertyInfo propertyInfo)
-        {
-            var columnAttribute = propertyInfo.GetAttributeOf<ColumnAttribute>();
-
-            return columnAttribute != null && columnAttribute.IsPrimaryKey;
-        }
-
-        public static T GetAttributeOf<T>(this PropertyInfo propertyInfo)
-        {
-            var attributes = propertyInfo.GetCustomAttributes(typeof(T), true);
-            if (attributes.Length == 0)
-            {
-                return default(T);
-            }
-            return (T)attributes[0];
-        }
-
-        [Serializable]
-        public class PrimaryKeyNotFoundException : Exception
-        {
-            public PrimaryKeyNotFoundException() { }
-
-            public PrimaryKeyNotFoundException(string message) : base(message) { }
-
-            public PrimaryKeyNotFoundException(string message, Exception innerException) : base(message, innerException) { }
-
-            protected PrimaryKeyNotFoundException(SerializationInfo info, StreamingContext streamingContext) : base(info, streamingContext) { }
-        }
-
-        public static void ClearControls(Control controle)
-        {
-            foreach (Control ctr in controle.Controls)
-            {
-                if (ctr.HasControls())
-                {
-                    ClearControls(ctr);
-                }
-
-                if (ctr is TextBox)
-                {
-                    var tb = (TextBox)ctr;
-                    tb.Text = "";
-                }
-
-                if (ctr is DropDownList)
-                {
-                    var ddl = (DropDownList)ctr;
-                    ddl.DataBind();
-                }
-
-                if (ctr is HtmlInputText)
-                {
-                    var hit = (HtmlInputText)ctr;
-                    hit.Value = "";
-                }
-            }
-        }
-
     }
 }
